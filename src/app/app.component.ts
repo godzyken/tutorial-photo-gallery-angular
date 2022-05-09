463cc24bb4f0b074da944f761d6ed8d87a328401
godzyken <isgodzy@gmail.com>
2022-05-09 17:15:01 +0200

    gradle error resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { SplashScreen } from '@capacitor/splash-screen'
+import { ElectronService } from 'ngx-electron';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor() {
+  constructor(private _electronService: ElectronService) {
     this.initializeApp();
   }
 
-  initializeApp() {
+  initializeApp(): void {
     /* To make sure we provide the fastest app loading experience
        for our users, hide the splash screen automatically
        when the app is ready to be used:
@@ -18,5 +19,11 @@ export class AppComponent {
         https://capacitor.ionicframework.com/docs/apis/splash-screen#hiding-the-splash-screen
     */
     SplashScreen.hide();
+    if (this._electronService.isElectronApp) {
+      let pong: string = this._electronService
+      .ipcRenderer
+      .sendSync('ping');
+      console.log(pong);
+    }
   }
 }
